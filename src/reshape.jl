2d70fbdc7e53832b94760543204e0d53d4df7f37
Aaron A. King <kingaa@umich.edu>
2024-12-05 07:04:16 -0500

    small tweaks to 'val_array'

--- src/reshape.jl
+++ src/reshape.jl
@@ -2,7 +2,7 @@ val_array(x::Vector) = x
 
 val_array(x::Array{X,N}) where {X,N} = vec(x)
 
-val_array(x::Array{X,N}, dim...) where {X,N} = let
+val_array(x::Array{X,N}, dim::Vararg{Integer}) where {X,N} = let
     q,r = divrem(length(x),prod(dim))
     if r != 0
         error("in `val_array`: size mismatch.")
@@ -13,7 +13,4 @@ end
 
 val_array(x) = [x]
 
-val_array(x, dim...) = begin
-    reshape([x],dim...)
-end
-
+val_array(x, dim::Vararg{Integer}) = reshape([x],dim...)
